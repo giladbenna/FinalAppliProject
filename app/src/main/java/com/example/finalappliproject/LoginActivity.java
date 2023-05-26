0847ef08157df34cf81f01f0bee8bf01d3030697
Gilad-PC <giladbennatan@gmail.com>
2023-05-26 17:13:04 +0300

    new logo design

--- app/src/main/java/com/example/finalappliproject/LoginActivity.java
+++ app/src/main/java/com/example/finalappliproject/LoginActivity.java
@@ -28,14 +28,27 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findViews();
+
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         getSupportActionBar().hide();
         adapter = new LoginAdapter(this);
         viewPager.setAdapter(adapter);
-
         new TabLayoutMediator(tabLayout,viewPager,((tab, position) -> tab.setText(titles[position]))).attach();
+
+        animation();
+    }
+
+
+    private void findViews(){
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        google = findViewById(R.id.fab_google);
+        fb = findViewById(R.id.fab_fb);
+    }
+
+    private void animation(){
         fb.setTranslationY(300);
         google.setTranslationY(300);
         tabLayout.setTranslationY(300);
@@ -47,13 +60,5 @@ public class LoginActivity extends AppCompatActivity {
         fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
         google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
         tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
-    }
-
-
-    private void findViews(){
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
-        google = findViewById(R.id.fab_google);
-        fb = findViewById(R.id.fab_fb);
     }
 }
